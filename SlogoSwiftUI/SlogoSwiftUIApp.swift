243aaad559cb526c722ddefb556ae5d852266631
Vladislav Ryabtsev <vryabtsev@me.com>
2022-02-08 13:13:54 +0300

    Initial Commit

---
+++ SlogoSwiftUI/SlogoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlogoSwiftUIApp.swift
+//  SlogoSwiftUI
+//
+//  Created by Vladislav Ryabtsev on 08.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SlogoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
